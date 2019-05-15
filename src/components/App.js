bb97115ce14474be6a95f4803aa2e545ec54a00f
Tyler Ellingford <tyler@Rickys-MacBook-Pro.local>
2019-05-15 16:08:49 -0600

    completed afternoon project

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-
 import './App.css';
-
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post';
+import axios from 'axios';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      posts: []
+      posts: [],
+      baseURL: 'https://practiceapi.devmountain.com/api',
     };
 
     this.updatePost = this.updatePost.bind( this );
@@ -19,23 +20,51 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    axios
+      .get(`${this.state.baseURL}/posts`)
+      .then(res => {
+        this.setState({ posts: res.data });
+      })
+      .catch( err => alert(err));
   }
 
-  updatePost() {
-  
+  updatePost( id, text ){
+    axios
+      .put(`${ this.state.baseURL }/posts?id=${ id }`, { text })
+      .then( res => {
+        this.setState({ posts: res.data });
+      })
+      .catch( err => alert(err));
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios
+      .delete(`${this.state.baseURL}/posts?id=${ id }`)
+      .then( res => {
+        this.setState({ posts: res.data });
+      })
+      .catch( err => alert(err));
   }
 
-  createPost() {
-
+  createPost(text) {
+    axios
+      .post(`${this.state.baseURL}/posts`, { text })
+      .then( res => {
+        this.setState({ posts: res.data });
+      })
+      .catch( err => alert(err));
   }
 
   render() {
     const { posts } = this.state;
+    const mappedPosts = posts.map( post => (
+    <Post key={post.id} 
+          id={post.id} 
+          text={post.text} 
+          date={post.date} 
+          updatePostFn={this.updatePost}
+          deletePostFn={this.deletePost} />
+    ));
 
     return (
       <div className="App__parent">
@@ -43,7 +72,9 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
+          <Compose createPostFn={this.createPost} />
+
+          {mappedPosts}
           
         </section>
       </div>
